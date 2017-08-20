49040ff0d2a329172bf4b82abf0bcc2dc168dd0a
Allison Marie <aliederman@icloud.com>
2017-08-20 17:30:38 -0400

    BasicCard functionallity

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,24 @@
-function BasicCard(front, back) {
+var inquirer = require('inquirer');
+var fs = require('fs');
+var chalk = require('chalk');
+
+var BasicCard = function (front, back) {
     this.front = front;
     this.back = back;
+};
+
+inquirer.prompt([{
+        type: 'input',
+        name: 'front',
+        message: 'Enter question to be displayed on flash card.'
+}, {
+        type: 'input',
+        name: 'back',
+        message: 'Enter answer to the question to display on the back of the flash card.'
 }
+]).then(function (toPrint) {
+    newBasicCard = new BasicCard(toPrint.front, toPrint.back);
+    console.log(chalk.green(newBasicCard));
+})
 
 module.exports = BasicCard;
